31cbbff5970e38b7065db7efbd7a70ffb6f52dd2
Gabriel Fiami <gabrielfiami.dev@gmail.com>
2022-12-16 18:58:08 -0300

    change whole computer choice to a function

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const Options = new Array("Rock", "Paper", "Scissors");
-function randomIndex() {
+let computerChoice;
+function randomComputerChoice() {
   I = Math.floor(Math.random() * 3);
-  return I;
+  computerChoice = Options[I];
+  console.log(computerChoice);
 }
-console.log(Options[randomIndex()]);
-let computerChoice = console.log(Options[randomIndex()]);
