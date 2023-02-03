61704c053955a79bc3a99092a36ef6151df708a0
Gabriel Fiami <gabrielfiami.dev@gmail.com>
2023-02-03 18:42:59 -0300

    transfer game from console to web screen, showing results and selections

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
 const Options = new Array("Rock", "Paper", "Scissors");
+const result = document.querySelector(".results");
+
 let computerChoise;
 let playerChoise;
+let results;
 
 let buttons = document.querySelectorAll(".playerChoise")
 for(i=0; i<buttons.length; i++){
   buttons[i].addEventListener('click', gameSettings)
 }
 function gameSettings(){
-  gameRound(randomComputerChoise(), setPlayerChoise(this))
+  result.innerHTML = ""
+  results = gameRound(randomComputerChoise(), setPlayerChoise(this))
+  showResults(results)
 }
 function setPlayerChoise(choise){
   playerChoise = choise.id
@@ -21,32 +26,34 @@ function randomComputerChoise() {
 }
 
 function gameRound(computer, player) {
-  console.log(`You choose ${player}`)
-  console.log(`Computer choose ${computer}`);
   if (computer == player) {
-    console.log("It's a tie!");
+    return "It's a tie!"
   } else {
     //computer == rock
     if (computer == "Rock" && player == "Paper") {
-      console.log("You won! Paper beats Rock");
+      return "Player won! Paper beats Rock"
     }
     if (computer == "Rock" && player == "Scissors") {
-      console.log("You lose! Rock beats Scissors");
+      return "Computer won! Rock beats Scissors"
     }
     //computer == paper
     if (computer == "Paper" && player == "Rock") {
-      console.log("You lose! Paper beats Rock");
+      return "Computer won! Paper beats Rock"
     }
     if (computer == "Paper" && player == "Scissors") {
-      console.log("You lose! Rock beats Scissors");
+      return "Computer won! Rock beats Scissors"
     }
     //computer == scissors
     if (computer == "Scissors" && player == "Rock") {
-      console.log("You won! Rock beats Scissors");
+      return "Player won! Rock beats Scissors"
     }
     if (computer == "Scissors" && player == "Paper") {
-      console.log("You lose! Scissors beats Paper");
+      return "Computer won! Scissors beats Paper"
     }
   }
 }
 
+function showResults(results){
+  result.innerHTML += `<BR> Player choose ${playerChoise} <BR> Computer choose ${computerChoise}<BR>` 
+  result.innerHTML += `${results}`
+}
